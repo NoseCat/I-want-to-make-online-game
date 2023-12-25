f57d17762b007bd9099e50354f854c9458dce444
NoseCat <zhirnovvladislav99@gmail.com>
2023-12-25 16:44:56 +0300

    Small updates to pistol

--- Entity Scenes/GunRoot.cs
+++ Entity Scenes/GunRoot.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class GunRoot : Node2D
 {
+	public bool reloading = false;
+	public Label label;
 	public AnimationPlayer Anima;
 	public Sprite2D Sprite;
 	public Marker2D Marker;
@@ -10,10 +12,16 @@ public partial class GunRoot : Node2D
 	public Marker2D Marker2;
 	public Vector2 Mouse; 
 	public Node2D Parent; 
+	[Export] uint ShotSpeed = 10;
+	public uint ShotProgress = 0;
+	public bool Shoot = false;
 	[Export] PackedScene BulletScene;
 	[Export] float BulletSpeed = 300;
+	[Export] uint max_ammo = 16;
+	public uint ammo = 16;
 	public override void _Ready()
 	{
+		label = GetNode<Label>("Gun/Label");
 		Anima = GetNode<AnimationPlayer>("AnimationPlayer");
 		Parent = GetNode<Node2D>("..");
 		Sprite = GetNode<Sprite2D>("Gun");
@@ -33,30 +41,58 @@ public partial class GunRoot : Node2D
 		Sprite.FlipH = false;
 	}
 
-	//private void AnimaFinish(string anim_name)
-	//{
-	//	if(anim_name != "RESET")
-	//		Anima.Play("RESET");
-	//}
+	private void AnimaFinish(string anim_name)
+	{
+		if(anim_name == "Reload" || anim_name == "Reload2")
+		{
+			ammo = max_ammo;
+			reloading = false;
+			label.Text = ammo + " / " + max_ammo;
+		}
+	}
 	
 	public override void _Process(double delta)
 	{
+		if ( (ammo <= 0 && Input.IsActionPressed("click") || Input.IsActionJustPressed("Reload")) && !reloading)
+		{
+			reloading = true;
+			Anima.Play("RESET");
+			Anima.Play(Sprite.FlipV ? "Reload" : "Reload2");
+			//Anima.Play("Reload");
+		}
+
+		//gun rotation
 		Mouse = GetGlobalMousePosition();
 		LookAt(Mouse);
 		if(Mouse.X < Parent.GlobalPosition.X && !Sprite.FlipV)
 		{
+			label.Rotation += (float)Math.PI;
+			label.Position *= new Vector2(1,-1);
+			label.Position += new Vector2(47/2,0);
 			Sprite.FlipV = true;
 			GetNode<Marker2D>("Marker2D3").Position *= new Vector2(1,-1);//= new Vector2(GetNode<Marker2D>("Marker2D3").Position.X, -GetNode<Marker2D>("Marker2D3").Position.Y);
 			Marker = Marker2;
 		}
 		if(Mouse.X >= Parent.GlobalPosition.X && Sprite.FlipV)
 		{
+			label.Rotation -= (float)Math.PI;
+			label.Position *= new Vector2(1,-1);
+			label.Position -= new Vector2(47/2,0);
 			Sprite.FlipV = false;
 			GetNode<Marker2D>("Marker2D3").Position *= new Vector2(1,-1); //= new Vector2(GetNode<Marker2D>("Marker2D3").Position.X, -GetNode<Marker2D>("Marker2D3").Position.Y);
 			Marker = Marker1;
 		}
-		if(Input.IsActionJustPressed("click"))
+
+		//shot	
+		if(ShotProgress == ShotSpeed)
 		{
+			ShotProgress = 0;
+		}
+
+		if(ShotProgress == 0 && ammo > 0 && !reloading && Input.IsActionPressed("click"))
+		{
+			ammo--;
+			label.Text = ammo + " / " + max_ammo;
 			Anima.Play("RESET");
 			Anima.Play(Sprite.FlipV ? "Shot2" : "Shot");
 			RigidBody2D bullet = BulletScene.Instantiate<RigidBody2D>();
@@ -67,5 +103,9 @@ public partial class GunRoot : Node2D
 			((boolet)bullet).speed = BulletSpeed;
 			GetTree().Root.AddChild(bullet);
 		}
+		if (Input.IsActionPressed("click"))
+			ShotProgress++;
+		else
+			ShotProgress = 0;
 	}
 }
